3997ec8d0f416503c264d3770ac50f99c760a844
Aevum Sonne <aevum.sonne@gmail.com>
2020-04-12 18:34:12 +0300

    Remove Exception, move util functions to Multiplier

--- lw1/MultMatrix/Multiplier/Multiplier.cpp
+++ lw1/MultMatrix/Multiplier/Multiplier.cpp
@@ -1,11 +1,11 @@
 #include "Multiplier.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
 Matrix3x3::matrix_t Multiplier(Matrix3x3::matrix_t firstMatrix3x3, Matrix3x3::matrix_t secondMatrix)
 {
     Matrix3x3 resultMatrix;
-    float sum;
-
     for (size_t row = 0; row != Matrix3x3::SIZE; row++)
     {
         for (size_t col = 0; col != Matrix3x3::SIZE; col++)
@@ -18,5 +18,56 @@ Matrix3x3::matrix_t Multiplier(Matrix3x3::matrix_t firstMatrix3x3, Matrix3x3::ma
         }
     }
     
+    return resultMatrix.coefs;
+}
+
+void ReadMatrixFromStream(std::ifstream& inStream, Matrix3x3& matrix3x3)
+{
+    float currCoef = 0;
+    for (size_t row = 0; row != Matrix3x3::SIZE; row++)
+    {
+        for (size_t col = 0; col != Matrix3x3::SIZE; col++)
+        {
+            inStream >> currCoef;
+            matrix3x3.coefs[row][col] = currCoef;
+        }
+    }
+
+    inStream.close();
+}
+
+void WriteMatrixInStream(std::ofstream& outStream, Matrix3x3::matrix_t resultMatrix)
+{
+    std::ofstream fout(outputFileName);
+
+    for (size_t row = 0; row != Matrix3x3::SIZE; row++)
+    {
+        for (size_t col = 0; col != Matrix3x3::SIZE; col++)
+        {
+            fout << resultMatrix[row][col] << separator;
+        }
+
+        fout << std::endl;
+    }
+}
+
+Matrix3x3::matrix_t MultiplyMatricesFromFiles(std::string firstFileName, std::string secondFileName)
+{
+    std::ifstream firstFile(firstFileName);
+    if (!firstFile)
+    {
+        throw std::runtime_error(firstFileOpenFailed);
+    }
+
+    std::ifstream secondFile(secondFileName);
+    if (!secondFile)
+    {
+        throw std::runtime_error(secondFileOpenFailed);
+    }
+
+    Matrix3x3 firstMatrix, secondMatrix, resultMatrix;
+    ReadMatrixFromStream(firstFile, firstMatrix); ReadMatrixFromStream(secondFile, secondMatrix);
+    resultMatrix.coefs = Multiplier(firstMatrix.coefs, secondMatrix.coefs);
+
     return resultMatrix.coefs;
 }
--- lw1/MultMatrix/Multiplier/Multiplier.h
+++ lw1/MultMatrix/Multiplier/Multiplier.h
@@ -1,4 +1,15 @@
 #pragma once
+
 #include "../Matrix3x3/Matrix3x3.h"
 
+const std::string invalidArgumentsCount = "Invalid number of arguments. "
+                                          "Using: ./MultMatrix matrix1.txt matrix2.txt";
+const std::string firstFileOpenFailed = "Failed to open first file.";
+const std::string secondFileOpenFailed = "Failed to open first file.";
+const std::string separator = " ";
+const std::string outputFileName = "output.txt";
+
+void ReadMatrixFromStream(std::ifstream& inStream, Matrix3x3& matrix3x3);
+void WriteMatrixInStream(std::ofstream& outStream, Matrix3x3::matrix_t resultMatrix);
 Matrix3x3::matrix_t Multiplier(Matrix3x3::matrix_t firstMatrix3x3, Matrix3x3::matrix_t secondMatrix);
+Matrix3x3::matrix_t MultiplyMatricesFromFiles(std::string firstFileName, std::string secondFileName);
